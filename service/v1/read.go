0c4b3fa0088fac78a31fdf19539402d211396b64
jukas <jukas@wemade.com>
2024-12-29 16:35:01 +0900

    Feat: update README.md

--- service/v1/read.go
+++ service/v1/read.go
@@ -5,3 +5,7 @@ import "github.com/04Akaps/elasticSearch.git/types/request"
 func (v1 V1) ReadTest(req request.ReadTestRequest) {
 	v1.elasticSearch.ReadTest(req.Index, req.Key, req.Value)
 }
+
+func (v1 V1) ReadTweets() {
+
+}
